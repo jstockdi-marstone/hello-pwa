3307682536bc8e46f4f6b87731a9d4858857786d
Jon Stockdill <jstockdill@marstone.com>
2024-02-20 10:30:12 -0500

    Adding platform specific messaging

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+const installMessage = document.getElementById('install-message');
+
+// Detect platform and update message
+if (navigator.userAgent.match(/.(iPhone|iPod|iPad).*/) && 'ontouchstart' in document) {
+  installMessage.innerText = "Tap \"Share\" > \"Add to Home Screen\"";
+} else if (navigator.userAgent.match(/Android/i)) {
+  installMessage.innerText = "Tap the menu button > \"Add to Home Screen\"";
+} else {
+  installMessage.innerText = "Click the menu button and install as an app";
+}
+
+
